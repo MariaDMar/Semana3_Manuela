1be553b7637277b5dbb674ad9861d5d1660f0f05
Manuela Epe <manuelaepedesing@gmail.com>
2018-08-17 15:58:24 -0500

    Trabajo en clase

--- app/src/main/java/com/example/estudiante/ejerciciosemana3/MainActivity.java
+++ app/src/main/java/com/example/estudiante/ejerciciosemana3/MainActivity.java
@@ -2,12 +2,44 @@ package com.example.estudiante.ejerciciosemana3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //alt+enter para importar
+    EditText et_masa;
+    EditText et_altura;
+    TextView tv_imc;
+    Button btn_calcular;
+    TextView tv_resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_masa = findViewById(R.id.et_masa);
+        et_altura = findViewById(R.id.et_altura);
+        tv_imc = findViewById(R.id.tv_imc);
+        btn_calcular = findViewById(R.id.btn_calcular);
+        tv_resultado = findViewById(R.id.tv_resultado);
+
+        //control+espacio
+        btn_calcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String masa = et_masa.getText().toString();
+                int a = Integer.parseInt(masa);
+                //tv_imc.setText("Hola "+masa);
+                double newA = Math.pow(a, 2);
+                tv_resultado.setText("Hola " + newA);
+
+                Toast.makeText(MainActivity.this, "Hola "+ newA, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
